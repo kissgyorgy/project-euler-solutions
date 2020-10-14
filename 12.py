7b17a1a34017cd79f6d3b9aa86772374f1f7be2a
Kiss György <kissgyorgy@me.com>
2020-10-14 20:00:13 +0200

    Improve problem #12
    Only search dividers up to the square of the number.
    
    See explanation: https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
    
    "sqrt(x) * sqrt(x) = x. So if the two factors are the same, they're both the
    square root. If you make one factor bigger, you have to make the other factor
    smaller. This means that one of the two will always be less than or equal to
    sqrt(x), so you only have to search up to that point to find one of the two
    matching factors. You can then use x / fac1 to get fac2."

--- 12.py
+++ 12.py
@@ -1,16 +1,30 @@
+from math import sqrt
+from itertools import count
+
+
+def count_dividers(num):
+    # deduplication for perfect squares
+    factors = set()
+    # From: https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
+    largest_factor = int(sqrt(num)) + 1
+    for n in range(1, largest_factor):
+        if num % n == 0:
+            factors.add(n)
+            factors.add(num // n)
+    print(", dividers:", len(factors), flush=True)
+    return len(factors)
+
+
 def main():
     trinum = 0
-    for next_trinum in range(99999999999999999):
-        trinum += next_trinum
-        print("Next triangle number:", trinum, end="")
-        # count the dividers without 1 and trinum
-        dividers_num = sum(trinum % n == 0 for n in range(2, int(trinum / 2)))
-        dividers_num += 2  # add 1 and trinum itself
-        print(", dividers:", dividers_num)
-        if dividers_num > 500:
+    for next_int in count(1):
+        trinum += next_int
+        print("Triangle number:", trinum, end="", flush=True)
+        if count_dividers(trinum) > 500:
             return trinum
 
 
 if __name__ == "__main__":
     triangle_number = main()
     print("Result:", triangle_number)
+    # 76576500
