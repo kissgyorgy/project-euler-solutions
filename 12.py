1cbd81b295c40ed7c47cce4c474c70d990506658
Kiss György <kissgyorgy@me.com>
2020-10-14 18:42:26 +0200

    Speed up #12
    We don't need to try dividers half than the number itself...
    Still very slow.

--- 12.py
+++ 12.py
@@ -4,7 +4,7 @@ def main():
         trinum += next_trinum
         print("Next triangle number:", trinum, end="")
         # count the dividers without 1 and trinum
-        dividers_num = sum(trinum % n == 0 for n in range(2, trinum))
+        dividers_num = sum(trinum % n == 0 for n in range(2, int(trinum / 2)))
         dividers_num += 2  # add 1 and trinum itself
         print(", dividers:", dividers_num)
         if dividers_num > 500:
